53b5440c9e95f5798e4b5c864ec1b24240045448
Vigneshkrishnan0001 <110534882+Vigneshkrishnan0001@users.noreply.github.com>
2023-02-22 11:34:30 +0530

    Create index.js

---
+++ docker-use/index.js
@@ -0,0 +1,8 @@
+const express = require('express')
+const app = express()
+
+apt.get('/',(req/res) => res.send('custom dockerfile for a simple express nodejs webapp'));
+
+app.listen(3000, ()=>{
+    console.log('Listening on port 3000')
+})
